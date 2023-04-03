d92aa81077d1fa733b337048c68eb11e0035c43b
priyanshu kumar <114394901+priyanshu-rj@users.noreply.github.com>
2023-04-03 16:55:05 +0530

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,11 @@
+import qrcode
+qr=qrcode.QRCode(
+    version=15,
+    box_size=10,
+    border=5
+)
+data="input your link"
+qr.add_data(data)
+qr.make(fit=True)
+img=qr.make_image(fill="black",back_color="white")
+img.save('text.png')
